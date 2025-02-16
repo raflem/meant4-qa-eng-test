533e1d44dfbe6b7536d74c82578e627a864dacc0
lemiecr <rafal.lemiec@contractors.roche.com>
2025-02-16 20:18:57 +0100

    first steps

--- features/steps/test_login.py
+++ features/steps/test_login.py
@@ -1,4 +1,11 @@
-from pytest_bdd import scenario, given, when, then
+import pytest
+from pytest_bdd import scenario
+from pytest_bdd import given
+from pytest_bdd import when
+from pytest_bdd import then
+
+from meant4_qa_eng_test.ap_pom import LOGIN_PAGE
+from meant4_qa_eng_test.ap_pom import LOGIN_BUTTON
 
 
 @scenario('login.feature', 'Logging in')
@@ -6,29 +13,38 @@ def test_publish():
     pass
 
 
+@pytest.fixture(scope="session")
+def login_info():
+    return {}
+
+
 @given("Browser is open on the login page")
-def open_browser_and_page():
-    pass
+def open_browser_and_page(browser):
+    browser.visit(LOGIN_PAGE)
 
 
 @given("I have an account")
-def check_account():
-    pass  # ideally this is another prerequisite test or this function returns pre-made login info
+def check_account(login_info):
+    # having an account is a given for this test
+    login_info["email"] = "spoofed@account.com"
+    login_info["pswd"] = "pswd1234"
+    return login_info
 
 
 @when("I enter account name")
-def enter_account_name():
-    pass
+def enter_account_name(browser, login_info):
+    browser.fill("email", login_info["email"])
 
 
 @when("I enter password")
-def enter_password():
-    pass
+def enter_password(browser, login_info):
+    browser.fill("passwd", login_info["pswd"])
 
 
 @when("I press the 'log in' button")
-def press_login():
-    pass
+def press_login(browser):
+    button = browser.find_by_xpath(LOGIN_BUTTON)
+    browser.click(button)
 
 
 @then("I should be logged in")
---
+++ src/meant4_qa_eng_test/ap_pom.py
@@ -0,0 +1,3 @@
+HOME_PAGE = "http://www.automationpractice.pl/index.php"
+LOGIN_PAGE = "http://www.automationpractice.pl/index.php?controller=authentication&back=my-account"
+LOGIN_BUTTON = '//button[contains(text(), "Sign In")]'
