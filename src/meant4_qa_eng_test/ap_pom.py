12f950dd4843ed6fed4ba29d088a77c87422b380
raflem <rafalemiec@gmail.com>
2025-02-17 09:30:56 +0100

    updated reqs

--- src/meant4_qa_eng_test/ap_pom.py
+++ src/meant4_qa_eng_test/ap_pom.py
@@ -1,3 +1,6 @@
+"""
+Simple Page Object Model for the www.automationpractice.pl website
+"""
 HOME_PAGE = "http://www.automationpractice.pl/index.php"
 LOGIN_PAGE = "http://www.automationpractice.pl/index.php?controller=authentication&back=my-account"
 LOGIN_BUTTON = '//button[contains(text(), "Sign In")]'
